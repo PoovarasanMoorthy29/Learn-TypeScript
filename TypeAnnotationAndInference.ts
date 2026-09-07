0a84296120f215881655833d0afe6fc975e9a697
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-09-07 19:12:07 +0530

    Topic : TypeAnnotation and inferencce in typescript .

---
+++ TypeAnnotationAndInference.ts
@@ -0,0 +1,13 @@
+//1. Type Annotation
+// A type annotation means you explicitly tell TypeScript what type a variable should have.
+
+let age:number=12;
+console.log(age);
+
+
+// 2. Type Inference
+// Type inference means TypeScript automatically figures out the type from the value you assign.
+
+let cardNo=12334556;
+let characterName="Jon snow";
+console.log("card no: "+cardNo);
