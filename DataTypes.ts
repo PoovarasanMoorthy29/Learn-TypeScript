65026892b441c42d0552ffcc8f1fa78706156c5f
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-09-07 14:57:47 +0530

    add symbol datatype

--- DataTypes.ts
+++ DataTypes.ts
@@ -5,4 +5,6 @@ let age:number=21;// number includes float,integer,hexadecimal etc...
 let isCompleted : boolean =true;
 const myBigNumber=BigInt(1234567899);
 console.log(myBigNumber);
+const mySymbol = Symbol('hello');
+console.log(mySymbol)
 
